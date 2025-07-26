a4097ea8b7ca3daff197845a566d89714f2d8de1
srdjanmiljus <srmiljus@gmail.com>
2025-07-26 11:20:59 +0200

    added allure reporter, updated yaml for allure and .gitignore added readme.md empty file

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,10 +12,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['list'],
-    ['html', { outputFolder: './reports', open: 'always' }],
-  ],
+ reporter: [
+  ['list'],
+  ['html', { outputFolder: './reports', open: 'always' }],
+  ['allure-playwright'],
+],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
