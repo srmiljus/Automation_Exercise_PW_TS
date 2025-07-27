f7fb9ec69725e9b3662c0f786316e309ae6de9fc
srdjanmiljus <srmiljus@gmail.com>
2025-07-27 11:46:26 +0200

    refactored url's constants  and package json

--- tests/constants/urls.ts
+++ tests/constants/urls.ts
@@ -1,9 +1,10 @@
+// @ts-ignore
+const BASE_URL = process.env.BASE_URL;
+
 export const URLS = {
-  HOME: 'https://www.automationexercise.com/',
-  LOGIN: 'https://www.automationexercise.com/login',
-  SIGNUP: 'https://www.automationexercise.com/signup',
-  ACCOUNT_CREATED: 'https://www.automationexercise.com/account_created',
-  USER_DASHBOARD: 'https://www.automationexercise.com/user-dashboard',
-  PRODUCT: 'https://www.automationexercise.com/products',
-  CART: 'https://www.automationexercise.com/view_cart',
+  HOME: `${BASE_URL}/`,
+  SIGNUP: `${BASE_URL}/signup`,
+  ACCOUNT_CREATED: `${BASE_URL}/account_created`,
+  PRODUCT: `${BASE_URL}/products`,
+  CART: `${BASE_URL}/view_cart`,
 };
